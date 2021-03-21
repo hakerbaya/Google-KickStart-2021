5809181bf58d16c122e1b9100c84d190ca2764c5
Mubashir Ahmad <34230563+hakerbaya@users.noreply.github.com>
2021-03-21 12:58:13 +0530

    Add files via upload

---
+++ Round A/K-Goodness String.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main(){
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     
+     int t; cin >> t;
+     for(int tc = 1;tc <=t;tc++){
+         ll ans = 0,cnt=0;
+         ll len,K;cin >> len >> K;
+         string s; cin >> s;
+
+         for(ll i=0;i<len/2;i++){
+             if(s[i]!=s[(len-1)-i]){
+                //  cout << s[i] <<" "<<s[(len-1)-i] <<"\n";
+                 cnt++;
+             }
+         }
+         
+         ans = abs(K-cnt);
+         
+         cout << "Case #"<<tc<<": "<<ans<<"\n";
+    }
+}
